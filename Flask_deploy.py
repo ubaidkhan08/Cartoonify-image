4125a434ca885730f41e56c9a21352a4f0839225
ubaidkhan08 <88443025+ubaidkhan08@users.noreply.github.com>
2022-11-13 22:39:30 +0530

    Update Flask_deploy.py

--- Flask_deploy.py
+++ Flask_deploy.py
@@ -5,8 +5,6 @@ import imageio #to read image stored at particular path
 import sys
 import matplotlib.pyplot as plt
 import os
-import tkinter as tk
-
     
 def main():
     st.title("Cartoonify Your Image!")
