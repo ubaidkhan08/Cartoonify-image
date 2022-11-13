98c8ed6198914b11690fd6e17dab7d5ef3fb6ace
ubaidkhan08 <88443025+ubaidkhan08@users.noreply.github.com>
2022-11-13 18:30:07 +0530

    Update Flask_deploy.py

--- Flask_deploy.py
+++ Flask_deploy.py
@@ -1,6 +1,17 @@
+from joblib import dump, load
 import streamlit as st
 from joblib import dump, load
-import numpy as np
+import cv2 #for image processing
+import easygui #to open the filebox
+import numpy as np #to store image
+import imageio #to read image stored at particular path
+import sys
+import matplotlib.pyplot as plt
+import os
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import *
+from PIL import ImageTk, Image
     
 def main():
     st.title("Cartoonify Your Image!")
