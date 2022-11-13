e9467f1c9983826d4c77a1f68e0e2881088bde28
ubaidkhan08 <88443025+ubaidkhan08@users.noreply.github.com>
2022-11-13 16:22:19 +0530

    Update App.py

--- App.py
+++ App.py
@@ -5,47 +5,70 @@ import pandas as pd
 
 
 def main():
-   
-    st.title("Natural Gas Prediction Model!")
+    st.title("Titanic Passenger Survival Prediction Model")
     html_temp = """
     <div style="background-color:teal ;padding:20px">
     </div>
     """
+
+    st.subheader('Enter Your Age')
+    AGE = st.number_input('Age')
+
+
+    st.subheader('Enter Your Ticket Price in $')
+    FARE = st.slider('Fare', 0.0, 520.0)
+
+
+    st.subheader('Choose Your Gender')
+    S = st.radio('Gender', options=('Female','Male','Transgenger','Prefer not to say'))
+    if S == 'Female':
+        SEX = 1
+
+    elif S == 'Male':
+        SEX = 0
+
+    elif S == 'Transgenger':
+        SEX = 0.5
+
+    elif S == 'Prefer not to say':
+        SEX = 0.5
+
+
+
+    st.subheader('Choose Passenger Class')
+    C = st.radio('P-class',options=('1st','2nd','3rd'))
+
+    if C == '1st':
+        Pc = 1
+
+    elif C == '2nd':
+        Pc = 2
+
+    elif C == '3rd':
+        Pc = 3
+
     
-    st.subheader('Crude Oil Price')
-    COP = st.number_input('(in Dollars)')
+    st.subheader('Choose Port of Boarding the Titanic')
+    E = st.selectbox('Embarkment' ,options=('Cherbourg', 'Queenstown', 'Southampton'))
+    if E == 'Cherbourg':
+        EMB = 1
+
+    elif E == 'Queenstown':
+        EMB = 2 
+
+    elif E == 'Southampton':
+        EMB = 0 
 
 
-    st.subheader('US Dollar Index')
-    USDI = st.number_input('Enter')
+    st.subheader('No. of Siblings/Spouses Aboard the Ship')
+    SibSp = st.number_input('Siblings or Spouses')
 
 
-    st.subheader('Texas Temperature')
-    TT = st.slider('(In Fahrenheit)', 0.0, 110.0)
-
-
-    st.subheader('California Temperature')
-    CT = st.slider('(In Fahrenheit)', 0.0, 105.0)
+    st.subheader('No. of Parents/Children Aboard the Ship')
+    Parch = st.number_input('Parents or Children')
 
     
-    st.subheader('Natural Gas Production in the USA')
-    NGP = st.number_input('(in Trillion cubic feet)')
 
 
-    st.subheader('GDP of USA')
-    GDP = st.number_input('(in Billion Dollars)')
-
-
-    st.subheader('US Natural Gas Reserves')
-    NGR = st.number_input('(in Trillion cubic feet)',key=0)
-
-    st.subheader('US Natural Gas Consumption')
-    NGC = st.number_input('(in Trillion cubic feet)',key=1)
-
-    st.subheader('US Natural Gas Imports')
-    NGI = st.number_input('(in Trillion cubic feet)',key=2)
-    
-    
-    
 if __name__=='__main__':
     main()
