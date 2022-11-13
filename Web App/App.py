bd4b9fd660f8c865f19f45b6de6ba76f3dce8982
ubaidkhan08 <88443025+ubaidkhan08@users.noreply.github.com>
2022-11-13 14:44:46 +0530

    Add files via upload

---
+++ Web App/App.py
@@ -0,0 +1,89 @@
+import streamlit as st
+from joblib import dump, load
+import cv2 #for image processing
+import easygui #to open the filebox
+import numpy as np #to store image
+import imageio #to read image stored at particular path
+import sys
+import matplotlib.pyplot as plt
+import os
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import *
+from PIL import ImageTk, Image
+    
+def main():
+    st.title("Major Project")
+    html_temp = """
+    <div style="background-color:teal ;padding:10px">
+    <h2 style="color:white;text-align:center;">Iris Classification</h2>
+    </div>
+    """
+
+
+	ImagePath = st.file_uploader("Choose a file")    
+
+
+    def cartoonify(ImagePath):
+    
+	    originalmage = cv2.imread(ImagePath)
+	    originalmage = cv2.cvtColor(originalmage, cv2.COLOR_BGR2RGB)
+
+	    # confirm that image is chosen
+	    if originalmage is None:
+	        print("Can not find any image. Choose appropriate file")
+	        sys.exit()
+
+	    ReSized1 = cv2.resize(originalmage, (1700, 2078))
+	    #plt.imshow(ReSized1, cmap='gray')
+
+	    #converting an image to grayscale
+	    grayScaleImage = cv2.cvtColor(originalmage, cv2.COLOR_BGR2GRAY)
+	    ReSized2 = cv2.resize(grayScaleImage, (1700, 2078))
+	    #plt.imshow(ReSized2, cmap='gray')
+
+	    #applying median blur to smoothen an image
+	    smoothGrayScale = cv2.medianBlur(grayScaleImage, 5)
+	    ReSized3 = cv2.resize(smoothGrayScale, (1700, 2078))
+	    #plt.imshow(ReSized3, cmap='gray')
+
+	    #retrieving the edges for cartoon effect
+	    #by using thresholding technique
+	    getEdge = cv2.adaptiveThreshold(smoothGrayScale, 255, 
+	      cv2.ADAPTIVE_THRESH_MEAN_C, 
+	      cv2.THRESH_BINARY, 9, 9)
+
+	    ReSized4 = cv2.resize(getEdge, (1700, 2078))
+	    #plt.imshow(ReSized4, cmap='gray')
+
+	    #applying bilateral filter to remove noise 
+	    #and keep edge sharp as required
+	    colorImage = cv2.bilateralFilter(originalmage, 9, 300, 300)
+	    ReSized5 = cv2.resize(colorImage, (1700, 2078))
+	    #plt.imshow(ReSized5, cmap='gray')
+
+	    #masking edged image with our "BEAUTIFY" image
+	    cartoonImage = cv2.bitwise_and(colorImage, colorImage, mask=getEdge)
+
+	    ReSized6 = cv2.resize(cartoonImage, (1700, 2078))
+	    #plt.imshow(ReSized6, cmap='gray')
+
+	    # Plotting the whole transition
+	    images=[ReSized1, ReSized2, ReSized4, ReSized6]
+	    fig, axes = plt.subplots(2,2, figsize=(8,8), subplot_kw={'xticks':[], 'yticks':[]}, gridspec_kw=dict(hspace=0.1, wspace=0.1))
+	    for i, ax in enumerate(axes.flat):
+	        ax.imshow(images[i], cmap='gray')
+
+	    plt.show()
+   
+
+	if st.button('Cartoonify your image!'):
+        output = cartoonify(ImagePath)
+        st.success(output)
+
+    #st.image(ReSized6, caption='Here is your Cartooni-fied image!')
+
+
+        
+if __name__=='__main__':
+    main()
